8a259bddbd808c4a901ff50e3cccf347809891a1
Daniel Martinez <d.martinez@mail.ch>
2025-10-28 13:36:25 +0700

    0.2.1

--- Pizza-Programm/App.xaml.cs
+++ Pizza-Programm/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Globalization; // HINZUGEFÜGT
+using System.Threading;     // HINZUGEFÜGT
 
 namespace Pizza_Programm
 {
@@ -9,6 +11,22 @@ namespace Pizza_Programm
     /// </summary>
     public partial class App : Application
     {
-    }
+        // HINZUGEFÜGT: OnStartup-Methode
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            // Setzt die Standardkultur für die gesamte Anwendung auf Thai (Thailand)
+            var cultureInfo = new CultureInfo("th-TH");
+
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+
+            // Stellt sicher, dass WPF-Bindungen die neue Kultur verwenden
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+                typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(
+                    System.Windows.Markup.XmlLanguage.GetLanguage(cultureInfo.Name)));
+        }
+    }
 }
